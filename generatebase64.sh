d2d4a123e326b78611a2ececd0cadf7a5dcf4fd5
Michael <crystalcvo@gmail.com>
2023-04-27 21:41:55 +0000

    Initial commit

---
+++ generatebase64.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export APP_DB_PORTA_BASE64=$(echo -n "$APP_DB_PORTA" | base64 | tr -d \\n)
+export APP_DB_ROOT_PASSWORD_BASE64=$(echo -n "$APP_DB_ROOT_PASSWORD" | base64 | tr -d \\n)
+export APP_DB_ROOT_USERNAME_BASE64=$(echo -n "$APP_DB_ROOT_USERNAME" | base64 | tr -d \\n)
+export APP_DB_SEI_BASE_BASE64=$(echo -n "$APP_DB_SEI_BASE" | base64 | tr -d \\n)
+export APP_DB_SEI_PASSWORD_BASE64=$(echo -n "$APP_DB_SEI_PASSWORD" | base64 | tr -d \\n)
+export APP_DB_SEI_USERNAME_BASE64=$(echo -n "$APP_DB_SEI_USERNAME" | base64 | tr -d \\n)
+export APP_DB_SIP_BASE_BASE64=$(echo -n "$APP_DB_SIP_BASE" | base64 | tr -d \\n)
+export APP_DB_SIP_PASSWORD_BASE64=$(echo -n "$APP_DB_SIP_PASSWORD" | base64 | tr -d \\n)
+export APP_DB_SIP_USERNAME_BASE64=$(echo -n "$APP_DB_SIP_USERNAME" | base64 | tr -d \\n)
+export APP_MAIL_SENHA_BASE64=$(echo -n "$APP_MAIL_SENHA" | base64 | tr -d \\n)
+export APP_MAIL_USUARIO_BASE64=$(echo -n "$APP_MAIL_USUARIO" | base64 | tr -d \\n)
+export APP_SEI_CHAVE_ACESSO_BASE64=$(echo -n "$APP_SEI_CHAVE_ACESSO" | base64 | tr -d \\n)
+export APP_SIP_CHAVE_ACESSO_BASE64=$(echo -n "$APP_SIP_CHAVE_ACESSO" | base64 | tr -d \\n)
+export OPENLDAP_ADMIN_PASSWORD_BASE64=$(echo -n "$OPENLDAP_ADMIN_PASSWORD" | base64 | tr -d \\n)
+envsubst < orquestrators/rancher-kubernetes/templates/secrets-template.yaml > orquestrators/rancher-kubernetes/topublish/secrets.yaml
